c503b04830a4c9066087ca931637ffed4b221e95
cicada375 <ama20040207@gmail.com>
2022-11-03 20:05:06 +0200

    cleanup

--- app/src/main/java/com/example/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/myapplication/MapsActivity.java
@@ -26,7 +26,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     FusedLocationProviderClient fusedLocationProviderClient;
-    private int ACCESS_LOCATION_REQUEST_CODE = 10001;
+    private final int ACCESS_LOCATION_REQUEST_CODE = 10001;
     LocationRequest locationRequest;
     GoogleMap googleMap;
 
@@ -44,15 +44,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        // Add a marker in Sydney and move the camera
-        //MapStyleOptions styleOptions=MapStyleOptions.loadRawResourceStyle(this, googleMap.setMapStyle(GoogleMap.MAP_TYPE_SATELLITE));
-        //LatLng sydney = new LatLng(-34, 151);
-
         googleMap.setMinZoomPreference(7.5f);
-
-        //googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        //googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        //new MarkerOptions().position(new LatLng(49.0811115,28.3440991))
         LatLng Vinnitsia = new LatLng(49.102275, 28.675528);
 
 
@@ -76,22 +68,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         googleMap.setMyLocationEnabled(true);
 
 
-        //googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        //googleMap.setMapStyle(styleOptions);
-
     }
 
     private void enableUserLocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-
-            // for ActivityCompat#requestPermissions for more details.
-
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
         }
         googleMap.setMyLocationEnabled(true);
     }

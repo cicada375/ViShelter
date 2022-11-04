0e928ce3b4141a5f1f08af5e0c7e5ae57bc36206
cicada375 <ama20040207@gmail.com>
2022-11-04 13:51:34 +0200

    я перейменував сергія чернюка в сергія чорного як завжди в принципі

--- app/src/main/java/com/example/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/myapplication/MapsActivity.java
@@ -9,9 +9,13 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.LocationRequest;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -27,6 +31,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private final int ACCESS_LOCATION_REQUEST_CODE = 10001;
     //LocationRequest locationRequest;
     GoogleMap googleMap;
+    Button button;
+    Button button2;
+    Button button3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,14 +47,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+        button=findViewById(R.id.button);
+        button2=findViewById(R.id.button2);
+        button3=findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent=new Intent(getBaseContext(),); кароче тут треба визначити функціонал кнопок бо я цейво забув
+                //startActivity(intent) і так само з 3-4ма іншими кнопками
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {//тут настройки будуть відкриватися тому нове вікно буду робити
+            @Override
+            public void onClick(View view) {
+                //Intent intent=new Intent(getBaseContext(),);
+                //startActivity(intent);
+            }
+        });
         googleMap.setMinZoomPreference(7.5f);
         LatLng Vinnitsia = new LatLng(49.102275, 28.675528);
-
 
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(Vinnitsia));
         googleMap.addMarker(new MarkerOptions().position(Vinnitsia).title("Вінниця"));
         MapStyleOptions styleOptions = MapStyleOptions.loadRawResourceStyle(this, R.raw.custom_map_style3);//custom_map_style - дефолт, 2- ніч, 3- срібло, 4- темна тема, 5- Aubergine (синя тема)
-        googleMap.setMapStyle(styleOptions);
+        googleMap.setMapStyle(styleOptions);//
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(49.3084452, 28.4944493)));
         //цейво, штука щоб синій кружок був на нашій геолокації
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
@@ -71,7 +97,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
         googleMap.setMyLocationEnabled(true);
     }
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -85,3 +110,4 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 }
+
